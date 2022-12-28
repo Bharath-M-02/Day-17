801ee912bc6c3efbb1af96184ff15df59e4c6173
Bharath M <97436803+Bharath-M-02@users.noreply.github.com>
2022-12-28 23:50:21 +0530

    Add files via upload

---
+++ Callback Hell/script.js
@@ -0,0 +1,35 @@
+const root = document.querySelector(".root");
+
+setTimeout(function () {
+  root.innerText = "10";
+  setTimeout(function () {
+    root.innerText = "9";
+    setTimeout(function () {
+      root.innerText = "8";
+      setTimeout(function () {
+        root.innerText = "7";
+        setTimeout(function () {
+          root.innerText = "6";
+          setTimeout(function () {
+            root.innerText = "5";
+            setTimeout(function () {
+              root.innerText = "4";
+              setTimeout(function () {
+                root.innerText = "3";
+                setTimeout(function () {
+                  root.innerText = "2";
+                  setTimeout(function () {
+                    root.innerText = "1";
+                    setTimeout(function () {
+                      root.innerText = "Happy Independence Day!";
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
